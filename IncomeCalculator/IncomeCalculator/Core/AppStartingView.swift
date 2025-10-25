b7ee3411c4a520db59f8d110f8e0e6cd583f2cd8
Mohamed.Bamoh <mbamoh@icloud.com>
2025-10-25 12:48:51 +0200

    init project structure & assets

---
+++ IncomeCalculator/IncomeCalculator/Core/AppStartingView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  IncomeCalculator
+//
+//  Created by mohamedbamoh on 25.10.25.
+//
+
+import SwiftUI
+
+struct AppStartingView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    AppStartingView()
+}
---
+++ IncomeCalculator/IncomeCalculator/IncomeCalculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  IncomeCalculatorApp.swift
+//  IncomeCalculator
+//
+//  Created by mohamedbamoh on 25.10.25.
+//
+
+import SwiftUI
+
+@main
+struct IncomeCalculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            AppStartingView()
+        }
+    }
+}
